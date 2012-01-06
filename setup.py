5e6a8f1efa018aad95de11b084f20dab207ade6b
ram <ramusus@gmail.com>
2012-01-06 21:30:23 +0600

    added setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-oauth-tokens',
+    version=__import__('oauth_tokens').__version__,
+    description='Application for getting, refreshing and storing OAuth access_tokens for Django standalone applications',
+    long_description=open('README.rst').read(),
+    author='ramusus',
+    author_email='ramusus@gmail.com',
+    url='https://github.com/ramusus/django-oauth-tokens',
+    download_url='http://pypi.python.org/pypi/django-oauth-tokens',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False, # because we're including media that Django needs
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
